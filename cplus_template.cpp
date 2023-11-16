01fe67da329b662611982ceddec3cc52af6da06b
shogun187 <53176307+shogun187@users.noreply.github.com>
2023-11-16 13:10:53 -0800

    Add files via upload

--- cplus_template.cpp
+++ cplus_template.cpp
@@ -1,5 +1,63 @@
-//
-// Created by Shaugn Tan on 16/11/23.
-//
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-#include "cplus_template.h"
+using namespace std;
+
+const int nsz = 200;
+
+int n, k;
+int dp[nsz][nsz][2];
+
+void inline init() {
+    for (int i = 0; i < nsz; ++i) {
+        for (int j = 0; j < nsz; ++j) {
+            for (int l = 0; l < 2; ++l) {
+                dp[i][j][l] = -1;
+            }
+        }
+    }
+}
+
+int inline narrow_art_gallery(vector<vector<int> > &values, int row, int to_close, int closed_last) {
+    if (row == n) {
+        return 0;
+    }
+
+    if (dp[row][to_close][closed_last] != -1) {
+        return dp[row][to_close][closed_last];
+    }
+
+    int result = narrow_art_gallery(values, row + 1, to_close, 0) + values[row][0];
+
+    if (to_close > 0) {
+        if (closed_last == 0) {
+            result = max(result, narrow_art_gallery(values, row + 1, to_close - 1, 1) + values[row][1]);
+        }
+    }
+
+    dp[row][to_close][closed_last] = result;
+    return result;
+}
+
+int main() {
+    while (true) {
+        cin >> n >> k;
+        if (n == 0 && k == 0) {
+            break;
+        }
+
+        vector<vector<int> >  values(n, vector<int>(2));
+
+        for (int i = 0; i < n; ++i) {
+            cin >> values[i][0] >> values[i][1];
+        }
+
+        init();
+
+        int result = narrow_art_gallery(values, 0, k, 0);
+        cout << result << endl;
+    }
+
+    return 0;
+}
